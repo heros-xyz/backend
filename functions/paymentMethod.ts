fb12a41ac04999d6054a4931ff7c652c7862ac3f
Nacho Althabe <nacho.althabe@gmail.com>
2023-05-17 11:37:18 -0300

    WIP

--- functions/paymentMethod.ts
+++ functions/paymentMethod.ts
@@ -26,34 +26,35 @@ exports.create = ref.onCreate(async (change) => {
     if (!userDoc || !userDoc.data()) return
     const customer = userDoc?.data()?.stripeCustomer
     if(!customer) return
-    let paymentMethod: Stripe.Response<Stripe.PaymentMethod>|undefined
-    let error:string|undefined
-    try {
-        const stripe = new Stripe(stripeSecret, {apiVersion: "2022-11-15"});
-        paymentMethod = await stripe.paymentMethods.create({
-            type: "card",
-            card: {
-                number: data.cardNumber,
-                cvc: data.cardCvc,
-                exp_month: data.cardExpMonth,
-                exp_year: data.cardExpYear
+
+    const stripe = new Stripe(stripeSecret, {apiVersion: "2022-11-15"});
+    return stripe.paymentMethods.create({
+        type: "card",
+        card: {
+            number: data.cardNumber,
+            cvc: data.cardCvc,
+            exp_month: data.cardExpMonth,
+            exp_year: data.cardExpYear
+        }
+    }).then((paymentMethod)=>
+        stripe.paymentMethods.attach(paymentMethod.id, {
+            customer
+        }).then(()=>
+            stripe.paymentMethods.retrieve(paymentMethod.id)
+        )
+    ).then((paymentMethod)=>
+        change.ref.set({
+            stripePayment: paymentMethod,
+            uid: data.uid,
+        })
+    ).catch(async (error: Error) => {
+        console.error("createPaymentMethods", error);
+        return change.ref.update({
+            error: {
+                name: error.name,
+                message: error.message,
             }
         })
-
-        await stripe.paymentMethods.attach(paymentMethod.id, {
-            customer
-        })
-
-        paymentMethod = await stripe.paymentMethods.retrieve(paymentMethod.id)
-    } catch (e: any) {
-        console.error("createPaymentMethods", e)
-        error = e.message
-    }
-
-    return change.ref.set({
-        stripePayment: paymentMethod,
-        uid: data.uid,
-        error,
     })
 })
 
